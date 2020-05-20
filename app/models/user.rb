97a147ea28e5872f3a8c246ac1551d336393da25
Moin <kmoin786@gmail.com>
2020-05-21 03:43:13 +0530

    Complete user-post association

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   validates :username, presence: true, length: { in: 3..10 }
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX, 
     message: 'Enter a valid e-mail address.'}
+  has_many :posts
 end
