8782e9f4ba3accc7a0907053c1dc8f770e0622ec
Moin <kmoin786@gmail.com>
2020-05-21 04:26:09 +0530

    add linter and readme

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ApplicationRecord
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  private_constant :VALID_EMAIL_REGEX
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  # public
   validates :name, presence: true
   validates :username, presence: true, length: { in: 3..10 }
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX, 
-    message: 'Enter a valid e-mail address.'}
+  validates :email,
+            presence: true,
+            format: {
+              with: VALID_EMAIL_REGEX,
+              message: 'Enter a valid e-mail address.'
+            }
   has_many :posts
   has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,31 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_20_221507) do
-
-  create_table "comments", force: :cascade do |t|
-    t.string "username"
-    t.string "body"
-    t.integer "user_id"
-    t.integer "post_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+ActiveRecord::Schema.define(version: 20_200_520_221_507) do
+  create_table 'comments', force: :cascade do |t|
+    t.string 'username'
+    t.string 'body'
+    t.integer 'user_id'
+    t.integer 'post_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'posts', force: :cascade do |t|
+    t.string 'title'
+    t.text 'body'
+    t.integer 'user_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "username"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'users', force: :cascade do |t|
+    t.string 'name'
+    t.string 'username'
+    t.string 'email'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
