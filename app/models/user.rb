823a22c35a8dd344385abe3cf2349679deb8ed78
A L V A R O <alvarop3000@gmail.com>
2020-05-20 16:48:59 -0500

    add user model

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, presence: true
+  validates :username, presence: true, length: { in: 3..10 }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX, 
+    message: 'Enter a valid e-mail address.'}
+end
